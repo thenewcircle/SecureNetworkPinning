9da8abd2424eef6b142ff97ee781f75eadc8ebb3
Gil Zhaiek <gilzhaiek@gmail.com>
2019-09-07 21:00:13 -0700

    +coroutines

--- app/src/main/java/com/example/android/securenetworkpinning/MainActivity.kt
+++ app/src/main/java/com/example/android/securenetworkpinning/MainActivity.kt
@@ -7,49 +7,42 @@ import android.util.Log
 import android.view.View
 import android.widget.RadioGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 
 class MainActivity : Activity() {
-    private var textView: TextView? = null
-    private var endpointOptions: RadioGroup? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.text_results)
-        endpointOptions = findViewById(R.id.options)
     }
 
     fun onRequestClick(v: View) {
-        val task = RequestTask()
-        when (endpointOptions!!.checkedRadioButtonId) {
-            R.id.option_google -> task.execute("news.google.com/rss")
-            R.id.option_httpbin -> task.execute("httpbin.org/xml")
+        when (options.checkedRadioButtonId) {
+            R.id.option_google -> requestTask("news.google.com/rss")
+            R.id.option_httpbin -> requestTask("httpbin.org/xml")
             else -> throw IllegalArgumentException("Invalid Option Selected")
         }
         setResultText(null)
     }
 
-    fun setResultText(text: CharSequence?) {
-        textView!!.text = text
+    private fun setResultText(text: CharSequence?) {
+        textResults.text = text
     }
 
-    private inner class RequestTask : AsyncTask<String, Int, String>() {
-
-        override fun doInBackground(vararg params: String): String {
-            var url = params[0]
-
-            return try {
-                url = "http://$url"
-                RequestManager.makeRequest(url)
+    private fun requestTask(url: String) {
+        GlobalScope.async {
+            try {
+                val result = RequestManager.makeRequest("http://$url")
+                withContext(Dispatchers.Main) {
+                    setResultText(result)
+                }
             } catch (e: Exception) {
                 Log.w("RequestTask", "Unable to make request", e)
                 "Error"
             }
-        }
-
-        override fun onPostExecute(result: String) {
-            setResultText(result)
         }
     }
 }
