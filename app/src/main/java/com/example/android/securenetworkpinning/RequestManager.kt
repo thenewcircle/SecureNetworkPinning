0cfb59f04cd8a2a7217e281d6a61e7b3f865c8d5
Gil Zhaiek <gilzhaiek@gmail.com>
2020-07-18 14:42:23 -0700

    Android 10 changes

--- app/src/main/java/com/example/android/securenetworkpinning/RequestManager.kt
+++ app/src/main/java/com/example/android/securenetworkpinning/RequestManager.kt
@@ -1,20 +1,18 @@
 package com.example.android.securenetworkpinning
 
-
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLConnection
 import java.nio.charset.Charset
+import kotlin.math.max
 
 object RequestManager {
-
     @Throws(IOException::class)
     fun makeRequest(endpoint: String): String {
         val url = URL(endpoint)
         val urlConnection = url.openConnection() as HttpURLConnection
-
         return parseResponse(urlConnection)
     }
 
@@ -25,16 +23,12 @@ object RequestManager {
         val contentLength = connection.contentLength
 
         val buffer = ByteArray(16384)
-
-        val length = if (contentLength > 0) contentLength else 0
+        val length = max(contentLength, 0)
         val out = ByteArrayOutputStream(length)
-
-        var read: Int = input.read(buffer)
-        while (read != -1) {
+        var read: Int
+        while (input.read(buffer).also { read = it } != -1) {
             out.write(buffer, 0, read)
-            read = input.read(buffer)
         }
-
         return String(out.toByteArray(), Charset.forName(encoding))
     }
 }
